e1e48e8b4685d8edd23818d33a41448def9c787e
lhj <lhj20561@gmail.com>
2026-02-22 12:38:18 +0900

    fix otree app structure for chat

---
+++ chat/model.py
@@ -0,0 +1,15 @@
+from otree.api import *
+
+class C(BaseConstants):
+    NAME_IN_URL = 'chat'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+class Subsession(BaseSubsession):
+    pass
+
+class Group(BaseGroup):
+    pass
+
+class Player(BasePlayer):
+    pass
---
+++ chat/pages.py
@@ -0,0 +1,6 @@
+from otree.api import *
+
+class Survey(Page):
+    pass
+
+page_sequence = [Survey]
